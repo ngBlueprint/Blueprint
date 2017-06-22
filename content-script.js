11ca777b83c9656a8d35f4efa35178e7224b104b
anohx1030 <anohx1030@gmail.com>
2017-06-22 15:52:05 -0700

    working version

--- content-script.js
+++ content-script.js
@@ -3,13 +3,13 @@ window.addEventListener('message', function (event) {
   if (event.source !== window) {
     return;
   }
-
+  
   var message = event.data;
 
   // Only accept messages that we know are ours
   if (typeof message !== 'object' || message === null) {
     return;
   }
-  // alert(JSON.stringify(message))
+
   chrome.runtime.sendMessage(message);
 });
